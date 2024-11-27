fee7ddc5af98ab04a4cdf598f3802699b5b02835
Brouse <unaiunai2004@gmail.com>
2024-11-27 19:12:50 +0100

    [UPDATE] Added controls to the cache

---
+++ public/js/cache-controls.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#playpause').click(function () {
+        console.log("ds"); // Added missing semicolon
+        $(this).find('.fa-play, .fa-pause')
+            .toggleClass('fa-play')
+            .toggleClass('fa-pause');
+    });
+});
