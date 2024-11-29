957ac17271af3dd3e78c619d5b998d2e5620e3c0
Brouse <unaiunai2004@gmail.com>
2024-11-29 16:22:52 +0100

    [UPDATE] Added modals to change the variables

--- public/js/cache-controls.js
+++ public/js/cache-controls.js
@@ -1,8 +1,15 @@
 ﻿$(document).ready(function () {
     $('#playpause').click(function () {
-        console.log("ds"); // Added missing semicolon
         $(this).find('.fa-play, .fa-pause')
             .toggleClass('fa-play')
             .toggleClass('fa-pause');
     });
+
+    $('.openmodal').click(function () {
+        console.log($(this));
+        let value = $(this).attr('modal-data');
+
+        $('#modalTitle').text("Change " + value);
+        $('#labelModalForm').text(value);
+    })
 });
