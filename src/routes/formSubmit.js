8596c7357f706f3d780eb1f739491eee749a4dd9
Brouse <unaiunai2004@gmail.com>
2024-11-29 16:54:58 +0100

    [UPDATE] Added option to get the form validation on server-side

---
+++ src/routes/formSubmit.js
@@ -0,0 +1,15 @@
+﻿import express from "express";
+
+const form = express.Router();
+
+/**
+ * Load th home page on the router, and render the pug file.
+ */
+form.post('/form', (req , res ) =>{
+    const { modalInput } = req.body;
+    console.log(modalInput);
+    res.redirect('/')
+})
+
+// Export the router
+export { form };
