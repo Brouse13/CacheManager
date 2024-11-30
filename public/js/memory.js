a62009a19374f194a1ba1f0d737869318b9f190b
Brouse <unaiunai2004@gmail.com>
2024-11-30 15:30:45 +0100

    [UPDATE] Added load memory container

---
+++ public/js/memory.js
@@ -0,0 +1,17 @@
+﻿memory = []
+
+startAddress = 0;
+memorySize = 128;
+memoryStep = 6;
+function __initMemory() {
+    let endAddress = startAddress + memorySize;
+    let maxValue = Math.pow(2, memoryStep)
+
+    for (let i = startAddress; i < endAddress; i+=memoryStep) {
+        let address = "0x" + ("0000" +i.toString(16)).slice(-4);
+        let value = Math.floor(Math.random() * maxValue);
+
+        memory.push({ address, value });
+        memoryRender({ address, value });
+    }
+}
---
+++ public/js/memoryRender.js
@@ -0,0 +1,7 @@
+﻿function memoryRender(element) {
+    let memory = $('#memory-table').find('tbody');
+    memory.append(`<tr class="text-center">
+                    <td>${element.address}</td>
+                    <td>${element.value}</td>
+                    </tr>`);
+}
