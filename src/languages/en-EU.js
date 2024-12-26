003f5abf3cf5f719400df388dea756e480e5008a
Brouse <unaiunai2004@gmail.com>
2024-12-26 18:46:47 +0100

    [UPDATE] Added missing translations

--- src/languages/en-EU.js
+++ src/languages/en-EU.js
@@ -7,5 +7,45 @@
     },
     'navbar': {
         'brand': "Memory Cache Simulator",
+    },
+    'access': {
+        'title': "Memory Access",
+        'r/w': "R/W",
+        'value': "Value",
+        'address': "Address",
+        'hit': "Hit",
+        'miss': "Miss",
+        'stats': {
+            'title': "Statistics",
+            'hits': "Hits",
+            'misses': "Misses",
+            'hitRate': "Hit rate",
+            'missRate': "Miss rate",
+            'total': "Total accesses",
+            'percentage': "Percentage"
+        },
+    },
+    'options': {
+        'memory': {
+            'title': "Cache options",
+        },
+        'cache': {
+            'title': "Cache options",
+        }
+    },
+    'tables': {
+        'cache1': "Cache L1",
+        'cache2': "Cache L2",
+        'cache3': "Cache L3",
+        'cache': {
+            'tag': "Tag",
+            'dirty': "Dirty",
+            'valid': "Valid"
+        },
+        'memory': {
+            'title': "Memory",
+            'address': "Address",
+            'value': "Value"
+        }
     }
 }
--- src/languages/es-CA.js
+++ src/languages/es-CA.js
@@ -6,6 +6,46 @@
         'english': "Anglés"
     },
     'navbar': {
-        'brand': "Simulador de Memoria Cache"
+        'brand': "Simulador de Memòria Cache"
+    },
+    'access': {
+        'title': "Acces a la memòria",
+        'r/w': "L/E",
+        'value': "Valor",
+        'address': "Direcció",
+        'hit': "Encert",
+        'miss': "Error",
+        'stats': {
+            'title': "Estadístiques",
+            'hits': "Encerts",
+            'misses': "Errors",
+            'hitRate': "Tasa d'encerts",
+            'missRate': "Tasa d'errors",
+            'total': "Total de accesos",
+            'percentage': "Percentatge"
+        },
+    },
+    'options': {
+        'memory': {
+            'title': "Opcions de la memòria",
+        },
+        'cache': {
+            'title': "Opcions de la cache",
+        }
+    },
+    'tables': {
+        'cache1': "Cache L1",
+        'cache2': "Cache L2",
+        'cache3': "Cache L3",
+        'cache': {
+            'tag': "Etiqueta",
+            'dirty': "Brut",
+            'valid': "Vàlid"
+        },
+        'memory': {
+            'title': "Memòria",
+            'address': "Direcció",
+            'value': "Valor"
+        }
     }
 }
